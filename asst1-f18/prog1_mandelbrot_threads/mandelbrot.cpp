b7a2548d2e26c6fe7f2f5e4d9d984c8998512290
GiganticRay <lc3202683603@gmail.com>
2021-08-03 10:23:46 +0800

    finished multi-thread mandelbrot Set

--- asst1-f18/prog1_mandelbrot_threads/mandelbrot.cpp
+++ asst1-f18/prog1_mandelbrot_threads/mandelbrot.cpp
@@ -126,8 +126,23 @@ void* workerThreadStart(void* threadArgs) {
     WorkerArgs* args = static_cast<WorkerArgs*>(threadArgs);
 
     // TODO: Implement worker thread here.
+    float dx = (args->x1 - args->x0) / args->width;
+    float dy = (args->y1 - args->y0) / args->height;
 
-    printf("Hello world from thread %d\n", args->threadId);
+    int startRow    = args->threadId * (args->width / args->numThreads);
+    int endRow      = (args->threadId+1) * (args->width / args->numThreads) < args->height ? (args->threadId+1) * (args->width / args->numThreads) : args->height;
+
+    for(int j = startRow; j < endRow; j++){
+        for(int i = 0; i < args->width; i++){
+            float x = args->x0 + i*dx;
+            float y = args->y0 + j*dy;
+
+            int index = (j*args->width + i);
+            args->output[index] = mandel(x, y, args->maxIterations);
+        }
+    }
+
+    // printf("Hello world from thread %d\n", args->threadId);
 
     return NULL;
 }
@@ -143,6 +158,7 @@ void mandelbrotThread(
     int width, int height,
     int maxIterations, int output[])
 {
+    // height euals 900 which can not evenly divide 32
     const static int MAX_THREADS = 32;
 
     if (numThreads > MAX_THREADS)
@@ -152,11 +168,20 @@ void mandelbrotThread(
     }
 
     pthread_t workers[MAX_THREADS];
-    WorkerArgs args[MAX_THREADS];
+    WorkerArgs args[MAX_THREADS];   // #Max_Threads seperate variable space for different thread.
 
     for (int i=0; i<numThreads; i++) {
         // TODO: Set thread arguments here.
-        args[i].threadId = i;
+        args[i].threadId    = i;
+        args[i].x0          = x0; 
+        args[i].x1          = x1;
+        args[i].y0          = y0;
+        args[i].y1          = y1;
+        args[i].width       = width;
+        args[i].height      = height;
+        args[i].maxIterations = maxIterations;
+        args[i].output      = output;
+        args[i].numThreads  = numThreads;
     }
 
     // Fire up the worker threads.  Note that numThreads-1 pthreads
